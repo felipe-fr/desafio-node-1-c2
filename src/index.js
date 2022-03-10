b53eb47b552027abaf108ed62cae4c110ebc8033
Felipe Ribeiro <felipe.fwribeiro@gmail.com>
2022-03-10 14:45:59 -0300

    Finalizando o desafio complementar 2

--- src/index.js
+++ src/index.js
@@ -22,23 +22,29 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
-  return response.json(repository);
+  repositories.push(repository)
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs} = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
-
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  if (title) {
+    repository.title = title
+  }
+  if (url) {
+    repository.url = url
+  }
+  if (techs) {
+    repository.techs = techs
+  }
 
   return response.json(repository);
 });
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,15 +66,16 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repository.likes = repository.likes + 1;
 
-  return response.json('likes');
+
+  return response.json(repository);
 });
 
 module.exports = app;
